f4b58cfa91ce7d13e5d6d393f688624e11384ee6
Nikita <lubimov.nikita@gmail.com>
2021-08-10 10:44:23 +0300

    remove wrong async function

--- js/Department.js
+++ js/Department.js
@@ -9,23 +9,4 @@ class Department {
     return this._address
   }
 
-  // setAddressPromise(address) {
-  //   if (typeof address === 'string') {
-  //     const promise = new Promise((resolve, reject) => {
-  //       setTimeout(() => {
-  //         if (this._officeRent) {
-  //           this._address = address
-  //           resolve(this._address)
-  //         } else {
-  //           reject(console.warn('You cannot change the address.'))
-  //         }
-  //       }, 2000)
-  //     })
-  //     promise
-  //       .then(response => console.log(`You have successfully changed address to: ${response}`))
-  //       .catch(response => response)
-  //   } else {
-  //     return new Error('invalid data type')
-  //   }
-  // }
 }
