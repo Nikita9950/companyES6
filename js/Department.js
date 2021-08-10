4635a35360f9ca944d80e33866b77f4ce190cba3
Nikita <72048187+Nikita9950@users.noreply.github.com>
2021-08-10 11:04:30 +0300

    add async function

--- js/Department.js
+++ js/Department.js
@@ -9,4 +9,23 @@ class Department {
     return this._address
   }
 
+  setAddressAsync(address) {
+    if (typeof address === 'string') {
+      const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (this._officeRent) {
+            this._address = address
+            resolve(this._address)
+          } else {
+            reject(console.warn('You cannot change the address.'))
+          }
+        }, 2000)
+      })
+      promise
+        .then(response => console.log(`New address: ${response}`))
+        .catch(response => response)
+    } else {
+      console.warn('invalid data type')
+    }
+  }
 }
